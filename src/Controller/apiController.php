f2044bd9c85221f33493a62eb05f38777391e51a
rafal <r.p.wojciechowski10@gmail.com>
2024-02-05 22:58:51 +0100

    working req and res ajax

--- src/Controller/apiController.php
+++ src/Controller/apiController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,19 +12,20 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class apiController extends AbstractController
 {
     #[Route('/api', name: 'main')]
-    public function test(EntityManagerInterface $entityManager): Response
+    public function test(EntityManagerInterface $entityManager, Request $request): Response
     {
 
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: *");
-        header("Access-Control-Allow-Methods: *");
-        header("Allow: *");
+        // $hehe = $request -> getContent;
+        $hehe = $request->getMethod();
+
 
         $user = $entityManager->getRepository(User::class)->find(1);
         $userMail = $user->getEmail();
 
         $response['data'] = $userMail;
         $response['test-data'] = ' asdasd asd<>?>?>?>';
+        $response['test-datae'] = json_decode($hehe);
+
         
         $data = json_encode($response);
 
