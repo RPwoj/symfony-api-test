098a2471e1be1ba37d735b6ad19d8d931e899bf9
rafal <r.p.wojciechowski10@gmail.com>
2024-02-11 17:59:33 +0100

    started work with register validator and login password validator

--- src/Controller/apiController.php
+++ src/Controller/apiController.php
@@ -8,23 +8,105 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class apiController extends AbstractController
 {
     #[Route('/api', name: 'main')]
-    public function test(EntityManagerInterface $entityManager, Request $request): Response
+    public function test(EntityManagerInterface $entityManager, Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
-
+        $response['data'] = null;
         $reqMethod = $request->getMethod();
-        $reqData = $request->getContent();
 
-        $user = $entityManager->getRepository(User::class)->find(1);
-        $userMail = $user->getEmail();
+        $reqData = null;
         
-        $response['data'] = $userMail;
-        $response['test-enconding-special-chars'] = ' asdasd asd<>?>?>?>';
-        $response['test-request-method'] = $reqMethod;
-        $response['test-data'] = json_decode($reqData);
+        if ($reqMethod != 'GET') {
+            $reqData = json_decode($request->getContent(), true);
+        }
+        $response['data'] = $reqData;
+        switch ($reqMethod) {
+            // Register
+            case 'PATCH':
+                $user = $entityManager->getRepository(User::class);
+                
+                $userCheck = $user->findOneBy(['email' => $reqData['email']]);
+                
+                if ($userCheck) {
+                    $response['data'] = 'User ' . $userCheck . ' alreqady exist';
+
+                } else {
+                    $canRegister = true;
+
+                    if (strlen($reqData['pass']) < 7) {
+                        $canRegister = false;
+                        $response['errorPass'] = 'passwords are different';
+                    }
+
+                    if ($reqData['pass'] != $reqData['passRep']) {
+                        $canRegister = false;
+                        $response['errorPassRep'] = 'hasłą się różnią';
+                    } 
+                    
+                    if ($reqData['inputAcc'] == false) {
+                        $canRegister = false;
+                        $response['errorAcc'] = 'You must accept the rules';
+                    } 
+                    
+                    if ($canRegister == true) {
+                        $newUser = new User;
+                        $newUser -> setEmail($reqData['email']);
+
+                        $hashedPassword = $passwordHasher->hashPassword(
+                            $newUser,
+                            $reqData['pass']
+                        );
+
+                        $newUser -> setPassword($hashedPassword);
+                        $entityManager->persist($newUser);
+                        $entityManager->flush();
+    
+                        $response['data'] = 'user ' . $reqData['email'] . ' has been registered';
+                    }
+                }
+                /* $response['test-enconding-special-chars'] = ' asdasd asd<>?>?>?>'; */
+                $response['test-request-method'] = $reqMethod;
+                // $response['test-data'] = json_decode($reqData);
+                break;
+            
+            // Login
+            case 'POST':
+                $user = $entityManager->getRepository(User::class);
+                $userCheck = $user->findOneBy(['email' => $reqData['email']]);
+
+                if ($userCheck) {
+
+                    $plaintextPassword = $reqData['pass'];
+
+                    if ($plaintextPassword == '') {
+                        $response['passinfo'] = 'password cant be empty';
+                    } elseif (!$passwordHasher->isPasswordValid($userCheck, $plaintextPassword)) {
+                        $response['passinfo'] = 'wrong-password';
+                    } else {
+                        $response['passinfo'] = 'logged';
+                    }
+
+                } else {
+                    $response['userInfo'] = 'użytkownik ' . $reqData['email'] . ' nie istnieje';
+                }
+                break;
+
+            case 'GET':
+
+                break;
+
+            case 'DELETE':
+                
+                break;
+
+        }
+
+       
         
         $data = json_encode($response);
 
